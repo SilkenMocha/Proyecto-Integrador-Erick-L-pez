187d1bca1bbbf57e8aba193ffcbd6016062fc7a9
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-11-18 13:37:11 -0600

    Titles

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 import streamlit as st
+import numpy as numpy
+import pandas as pd
 
+st.title ("Métodos Numéricos")
+st.subheader("Tercer Evaluacion Parcial")
+st.subheader("Erick López Saldviar — 348916")
