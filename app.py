348b115d7031a44d0546682615dc23301bc36ee8
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-11-18 13:49:30 -0600

    sdf

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import pandas as pd
 
 st.title ("Métodos Numéricos")
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns(3)
 with col1:
     st.subheader("Tercer Evaluacion Parcial")
 with col2:
-    st.write("Erick López Saldviar — 348916")
-
+    st.write("Erick López Saldviar")
+with col2:
+    st.write("348916")
 seleccion = st.selectbox("Seleccione una opción: ", ["Método Biseccion", "Método Newton", "Método Secante", "Método Falsa Posicion", "Método Cramer"])
 
 #if seleccion == "Método Biseccion": 
