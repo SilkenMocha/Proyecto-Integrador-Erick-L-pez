ad87b1cab65b3402ff63b39f163e0065467e3796
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-11-18 13:54:15 -0600

    sdf

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as numpy
 import pandas as pd
+#___________________________________
+import biseccion
 
 st.title ("Métodos Numéricos")
 
@@ -8,9 +10,12 @@ col1, col2 = st.columns(2)
 with col1:
     st.subheader("Tercer Evaluacion Parcial")
 with col2:
-    st.subheader("Erick López 348916")
+    st.subheader("Erick López - 348916")
 
 
 seleccion = st.selectbox("Seleccione una opción: ", ["Método Biseccion", "Método Newton", "Método Secante", "Método Falsa Posicion", "Método Cramer"])
 
+st.latex(x''' 
+x^3 - 6x^2 + 11x -6.1
+''')
 #if seleccion == "Método Biseccion": 
