ea4567d2534f0cc247f3679af9f694ba6ec9c67a
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-11-18 20:29:19 -0600

    sdf

--- biseccion.py
+++ biseccion.py
@@ -11,7 +11,7 @@ def biseccion():
         a = st.number_input('Valor de a: ', value= -2)
         b = st.number_input('Valor de b: ', value = 4)
         tol = st.number_input('Tolerancia: ', step = 1e-5, value = 0.001 )
-        n0 = st.number_input('Iteraciones: ', step = 1, value = 100)
+        n0 = st.number_input('Iteraciones: ', value = 100)
         calcular = st.form_submit_button('Calcular')
     
 
