e9c4f73ef14a2fe15bba5dbdbd4b3cc733f185e6
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-11-18 22:18:02 -0600

    Metodo secante

--- secante.py
+++ secante.py
@@ -1,14 +1,19 @@
 import math
 def secante():
     i=1
-    print("_________________________________")
-    n0 = float(input("Iteraciones: "))
-    x0 = float(input("a: "))
-    x1 = float(input("b: "))
-    tol = float(input("Tolerancia: "))
+    with st.form(key='calc_biseccion'):
+        x0 = st.number_input('Valor de a: ', value= 3)
+        x1 = st.number_input('Valor de b: ', value = 3.5)
+        tol = st.number_input('Tolerancia: ', format="%.4f", step = 1e-4, value = 0.001 )
+        n0 = st.number_input('Iteraciones: ', value = 100)
+        calcular = st.form_submit_button('Calcular')    
 
-    fx0 = pow(x0, 3)-(10*x0)-5
-    fx1 = pow(x1, 3)-(10*x1)-5
+    fx0 = pow(x0, 3) - (6 * pow(x0, 2)) + (11*x0) - 6.1
+    fx1 = pow(x1, 3) - (6 * pow(x1, 2)) + (11*x1) - 6.1
+
+    lista_xn = []
+    lista_e = []
+    lista_c = []    
 
     while i<n0:
         xn= x1 - ((fx1*(x1-x0)) / (fx1 - fx0))
@@ -16,22 +21,23 @@ def secante():
             print("Procedure completed successfully")
             break
         e = (xn-x1)/xn
-        print(str(i) + "  xn: " + str(xn) + "   e: " + str(e))
+        
+        lista_xn.append(xn)
+        lista_e.append(e)
+        lista_c.append(i)        
+        
         i=i+1
         x0 = x1
-        x1=xn
-        fx0=fx1
-        fx1=pow(xn, 3)-(10*xn)-5
+        x1 = xn
+        fx0 = fx1
+        fx1 = pow(x0, 3) - (6 * pow(xn, 2)) + (11*xn) - 6.1
+    
+    d = {'Xn':lista_xn, 'e':lista_e }
+    df = pd.DataFrame(data=d, index = lista_c )
+    st.table(df)    
+
     e = (xn - x1) / xn
     print("iteraciones: " + str(i))
     print("Raiz es igual a: " + str(xn))
     print("Error: " + str(e))
 
-secante()
-
-print("_________________________________")
-seguir = input("Continuar? y/n: ")
-while seguir == "y":
-    secante()
-    print("_________________________________")
-    seguir = input("Continuar? y/n: ")
