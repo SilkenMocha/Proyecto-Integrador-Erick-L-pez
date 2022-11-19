5aeb37718dedca62beb8139dabf7152f02664aea
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-11-18 20:22:48 -0600

    Quitando comentarios

--- biseccion.py
+++ biseccion.py
@@ -5,15 +5,8 @@ import numpy as np
 
 
 def biseccion():
-    #print("_________________________________")
-    #a = float(input("Valor de a: "))
-    #b = float(input("Valor de b: "))
     c = 0
-    #tol = float(input("Tolerancia: "))
     i = 1
-    #n0 = float(input("Iteraciones: "))
-    #print("_________________________________")
-
     with st.form(key='calc_biseccion'):
         a = st.number_input('Valor de a: ', value= -2)
         b = st.number_input('Valor de b: ', value = 4)
@@ -46,64 +39,29 @@ def biseccion():
                 e = abs(xn - x) / xn
                 x = xn
                 i = i + 1
-                #print("x: " + str(xn) + " e: " + str(e) + " c: " + str(c))
 
             lista_xn.append(xn)
             lista_e.append(e)
             lista_c.append(c)
 
-            st.write("x: " + str(xn) + " e: " + str(e) + " c: " + str(c))
-
             if e < tol:
-                #print("\nProcedure completed successfully\n")
-                
+               
                 st.write("\nProcedure completed successfully\n")
                 break
 
 
 
     else:
-        #print("No hay raíz")
         st.write ("No hay raiz")
     
-    #print("iteraciones: " + str(c))
-    #print("Raiz es igual a: " + str(xn))
-    #print("Error: " + str(e))
+
 
     st.write("iteraciones: " + str(c))
     st.write("Raiz es igual a: " + str(xn))
     st.write("Error: " + str(e))
     st.write(lista_xn, lista_e, lista_c)
 
-    #cols = st.columns(3)
-    #currentCol = 0    
-    #for i in lista_xn:
-        #cols[currentCol].metric('xn'+, i)
-        #currentCol = (currentCol + 1) % len(cols)
-    
-    col1, col2 = st.columns(2)
-    c = 1
-    with col1:
-        for i in lista_xn:
-            c = c+1
-            st.metric('xn ' + str(c), i)
-
-    with col2:
-        for i in lista_e:
-            st.metric('e', i)
-
     d = {'Xn':lista_xn, 'e':lista_e }
     df = pd.DataFrame(data=d, index = lista_c )
     st.table(df)
 
-            
-
-
-
-#biseccion()
-#print("_________________________________")
-#seguir = input("Continuar? y/n: ")
-#while seguir == "y":
-    #biseccion()
-    #print("_________________________________")
-    #seguir = input("Continuar? y/n: ")
