2004ec0ce88129b58a24bc6d9158bc84b56f8388
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-11-18 20:41:44 -0600

    cambios streamlit

--- newton.py
+++ newton.py
@@ -1,38 +1,52 @@
 import math
+import streamlit as st
+import pandas as pd
+import numpy as np
+
 def newton():
     i=1
-    print("_________________________________")
-    n0 = float(input("Iteraciones: "))
-    tol = float(input("Tolerancia: "))
+    #print("_________________________________")
+    #n0 = float(input("Iteraciones: "))
+    #tol = float(input("Tolerancia: "))
+    with st.form(key='calc_biseccion'):
+        tol = st.number_input('Tolerancia: ', format="%.4f", step = 1e-4, value = 0.001)
+        n0 = st.number_input('Iteraciones', value = 100)
+        calcular = st.form_submit_button('Calcular')
 
     x0 = 1
-    f0 = pow(x0, 3)+2*pow(x0, 2)+7*x0
-    g0 = 3*pow(x0, 2)+4*x0+7
+    f0 = pow(x0, 3) - (6 * pow(x0, 2)) + (11 * x0) - 6.1
+    g0 = 3*pow(x0, 2) - 12*x0 + 11
+
+    lista_xn = []
+    lista_e = []
+    lista_c = []    
 
     while i < n0:
         xn = x0 - (f0/g0)
 
         if abs(xn-x0) <= tol:
-            print("Procedure completed successfully")
+            st.write("Procedure completed successfully")
             break
         e = (xn-x0)
-        print(str(i) + "  xn: " + str(xn) + "   e: " + str(e))
         i = i+1
+        
+        #print(str(i) + "  xn: " + str(xn) + "   e: " + str(e))
+
+        lista_xn.append(xn)
+        lista_e.append(e)
+        lista_c.append(c)
 
         x0 = xn
-        f0 = pow(x0, 3)+2*pow(x0, 2)+7*x0
-        g0 = 3*pow(x0, 2)+4*x0+7
+        f0 = pow(x0, 3) - (6 * pow(x0, 2)) + (11 * x0) - 6.1
+        g0 = 3*pow(x0, 2) - 12*x0 + 11
 
+    d = {'Xn':lista_xn, 'e':lista_e }
+    df = pd.DataFrame(data=d, index = lista_c )
+    st.table(df)    
+    
     e = (xn-x0)
-    print("iteraciones: " + str(i))
-    print("Raiz es igual a: " + str(xn))
-    print("Error: " + str(e))
+    st.subheader("iteraciones: " + str(i))
+    st.subheader("Raiz es igual a: " + str(xn))
+    st.subheader("Error: " + str(e))
 
-newton()
 
-print("_________________________________")
-seguir = input("Continuar? y/n: ")
-while seguir == "y":
-    newton()
-    print("_________________________________")
-    seguir = input("Continuar? y/n: ")
