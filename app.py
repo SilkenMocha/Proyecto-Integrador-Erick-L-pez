a5ce1e7689ba537ec452f65ad53d8327efc4a624
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-11-18 22:53:24 -0600

    sdfsd

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import biseccion as bs
 import newton as nw
 import secante as sc
 import regulafalsi as fp
-import cramer as cr
+#import cramer as cr
 
 st.title ("Métodos Numéricos")
 
@@ -44,11 +44,11 @@ if seleccion == "Método Falsa Posicion":
     st.latex('''x^3 - 6x^2 + 11x -6.1''')
     fp.regulafalsi()
 
-if seleccion == "Método Cramer": 
-    st.subheader("Método de Cramer")
-    with col1:
-        st.latex('''x + y = 1''')
+#if seleccion == "Método Cramer": 
+    #st.subheader("Método de Cramer")
+    #with col1:
+        #st.latex('''x + y = 1''')
     
-    with col2:
-        st.latex('''x + 5y = 10''')
-    cr.cramer(A,b) 
+    #with col2:
+        #st.latex('''x + 5y = 10''')
+    #cr.cramer(A,b) 
