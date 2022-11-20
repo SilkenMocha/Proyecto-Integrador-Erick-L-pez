d8d364be741e90462279244a86ad5aae4441b9c1
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-11-19 18:55:37 -0600

    sfd

--- cramer.py
+++ cramer.py
@@ -7,23 +7,27 @@ def cramer(A,b):
   n = len(b)
   D = np.linalg.det(A)
   x = np.zeros(n)
-
+  c = 0
   lista_x = []
+  lista_c = []
   for k in range(n):
     Ak = A.copy()
     Ak[:,k] = b
     Dk = np.linalg.det(Ak)
     x[k] = Dk/D
+    c = c+1
 
     lista_x.append(round(x[k], 5))
+    lista_c.append(c)
   
   cols = st.columns(3)
   currentCol = 0    
-  c = 0
+
   for i in lista_x:
     cols[currentCol].metric('X' + str(i+1) , i)
     currentCol = (currentCol + 1) % len(cols)  
-
+  
+  st.write(lista_c)
 
   
 
