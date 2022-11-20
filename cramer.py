830e7198b386ef79e6a31f30cb71978b00d6e8a3
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-11-19 18:35:00 -0600

    sdf

--- cramer.py
+++ cramer.py
@@ -8,15 +8,16 @@ def cramer(A,b):
   D = np.linalg.det(A)
   x = np.zeros(n)
 
+  lista_x = []
   for k in range(n):
     Ak = A.copy()
     Ak[:,k] = b
     Dk = np.linalg.det(Ak)
     x[k] = Dk/D
 
-    
+    lista_x.append(x[k])
     st.subheader("x" + str(k+1) + "= "+ str(x[k]))
-    st.metric("x" + str(k+1) , x[k])
+    st.metric("X" + str(k+1) , x[k])
 
   
 
