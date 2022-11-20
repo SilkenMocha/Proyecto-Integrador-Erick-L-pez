a22175818aa34bfc8b3b04adc2b7adf794b2827a
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-11-19 18:27:48 -0600

    sdf

--- cramer.py
+++ cramer.py
@@ -15,16 +15,8 @@ def cramer(A,b):
     Ak[:,k] = b
     Dk = np.linalg.det(Ak)
     x[k] = Dk/D
-    k=k+1
 
-    st.write ("x", str(k), "= ", x[k])
-    
-    #st.metric("X"+str(k), x[k])
+    st.write ("x", k+1, "= ", x[k])
 
-  #cols = st.columns(3)
-  #currentCol = 0    
-  #for i in x[k]:
-    #cols[currentCol].metric('x', i)
-    #currentCol = (currentCol + 1) % len(cols)
   
 
