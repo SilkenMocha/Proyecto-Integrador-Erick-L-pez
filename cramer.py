b14976d38ed06592bc2eeadc37feeb555fde8f42
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-11-19 18:05:54 -0600

    sdf

--- cramer.py
+++ cramer.py
@@ -3,19 +3,18 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-A = np.array([ [1,1], [1,5]])
-b = np.array([1,10])
-
-
 def cramer(A,b):
   n = len(b)
   D = np.linalg.det(A)
   x = np.zeros(n)
+  A
+  b
 
   for k in range(n):
     Ak = A.copy()
     Ak[:,k] = b
     Dk = np.linalg.det(Ak)
     x[k] = Dk/D
-    print ("x", k+1, "= ", x[k])
+    st.write ("x", k+1, "= ", x[k])
+  
 
